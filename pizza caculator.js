5c2c0f78db51b1e043b91aa48af9fb244eece234
ali sasani <alisasani@live.com>
2018-09-05 12:59:47 +0200

    pizza calculator

---
+++ pizza caculator.js
@@ -0,0 +1,21 @@
+//ali sasani
+//pizza caculator
+
+var small = 7.50;
+var total = prompt("how many small pizz'a do you want?");
+
+
+document.write("Small pizza's =" + total);
+document.write("<br> Price = " + total * small);
+
+var medium = 9.50;
+var total = prompt("how many medium pizza's do you want?");
+
+document.write("<br><br> Medium pizza's =" + total);
+document.write("<br> Price = " + total * medium);
+
+var large = 12.00;
+var total = prompt("how many large pizza's do you want?");
+
+document.write("<br><br> Large pizza's = " + total);
+document.write("<br> Price = " + total * large);
