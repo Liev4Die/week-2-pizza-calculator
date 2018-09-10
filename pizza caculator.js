e4a5b1a7ed8b8807b1b03393f4056e01d5db71e2
ali sasani <alisasani@live.com>
2018-09-10 11:41:59 +0200

    total toegevoegt

--- pizza caculator.js
+++ pizza caculator.js
@@ -2,20 +2,23 @@
 //pizza caculator
 
 var small = 7.50;
-var total = prompt("how many small pizz'a do you want?");
-
-
-document.write("Small pizza's =" + total);
-document.write("<br> Price = " + total * small);
-
 var medium = 9.50;
-var total = prompt("how many medium pizza's do you want?");
-
-document.write("<br><br> Medium pizza's =" + total);
-document.write("<br> Price = " + total * medium);
-
 var large = 12.00;
-var total = prompt("how many large pizza's do you want?");
 
-document.write("<br><br> Large pizza's = " + total);
-document.write("<br> Price = " + total * large);
+
+var totalsmall = prompt("how many small pizz'a do you want?");
+document.write("Small pizza's =" + totalsmall);
+document.write("<br> Price = " + totalsmall * small);
+
+
+var totalmedium = prompt("how many medium pizza's do you want?");
+document.write("<br><br> Medium pizza's =" + totalmedium);
+document.write("<br> Price = " + totalmedium * medium);
+
+
+var totallarge = prompt("how many large pizza's do you want?");
+document.write("<br><br> Large pizza's = " + totallarge);
+document.write("<br> Price = " + totallarge * large);
+
+
+document.write('<br><br>Total price : ' + small * totalsmall + medium * totalmedium + large * totallarge);
