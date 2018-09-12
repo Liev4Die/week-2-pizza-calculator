f0addf6b8c9b4aceeba57b24dad530c771859bae
ali sasani <alisasani@live.com>
2018-09-12 09:19:03 +0200

    new var

--- pizza caculator.js
+++ pizza caculator.js
@@ -1,26 +1,25 @@
 //ali sasani
 //pizza caculator
 
-var small = 7.50;
-var medium = 9.50;
-var large = 12.00;
+var small = 7.50;// 7.50€
+var medium = 9.50;// 9.50€
+var large = 12.00;// 12.00€
+var smallprice = totalsmall * small;// 7.50€ x total small pizza
+var mediumprice = totalmedium * medium;// 9.50€ x total medium pizza
+var largeprice = totallarge * large;// 12.00€ x total large pizza
+var total = smallprice + mediumprice + largeprice;//total price
 
 
 var totalsmall = prompt("how many small pizz'a do you want?");
 document.write("Small pizza's =" + totalsmall);
-var smallprice = totalsmall * small;
 
 
 var totalmedium = prompt("how many medium pizza's do you want?");
 document.write("<br><br> Medium pizza's =" + totalmedium);
-var mediumprice = totalmedium * medium;
 
 
 var totallarge = prompt("how many large pizza's do you want?");
 document.write("<br><br> Large pizza's = " + totallarge);
-var largeprice = totallarge * large;
 
-
-var total = smallprice + mediumprice + largeprice;
 
 document.write('<br><br>Total price : ' + total);
