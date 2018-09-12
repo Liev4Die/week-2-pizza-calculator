bb6873fe966e99b67d678d1a500b5338d2361414
ali sasani <alisasani@live.com>
2018-09-12 10:12:14 +0200

    einde

--- pizza caculator.js
+++ pizza caculator.js
@@ -1,25 +1,29 @@
 //ali sasani
 //pizza caculator
 
-var small = 7.50;// 7.50€ small pizza
-var medium = 9.50;// 9.50€ medium pizza
-var large = 12.00;// 12.00€ large pizza
-var smallprice = totalsmall * small;// 7.50€ x total small pizza
-var mediumprice = totalmedium * medium;// 9.50€ x total medium pizza
-var largeprice = totallarge * large;// 12.00€ x total large pizza
-var total = smallprice + mediumprice + largeprice;//total price
+var small = 7.50;
+var medium = 9.50;
+var large = 12.00;
+var totalsmall = prompt("how many small pizz'a do you want?");
+var smallprice = totalsmall * small;
+var totalmedium = prompt("how many medium pizza's do you want?");
+var mediumprice = totalmedium * medium;
+var totallarge = prompt("how many large pizza's do you want?");
+var largeprice = totallarge * large;
+var total = smallprice + mediumprice + largeprice;
 
 
-var totalsmall = prompt("how many small pizz'a do you want?");//total small pizza
-document.write("Small pizza's =" + totalsmall);//total small pizza
+document.write(" Small pizza's =" + totalsmall);
+document.write("<br> total small pizza: " + smallprice);
 
 
-var totalmedium = prompt("how many medium pizza's do you want?");//total medium pizza
-document.write("<br><br> Medium pizza's =" + totalmedium);//total medium pizza
+document.write("<br><br> Medium pizza's =" + totalmedium);
+document.write("<br> total medium pizza: " + mediumprice);
 
 
-var totallarge = prompt("how many large pizza's do you want?");//total large pizza
-document.write("<br><br> Large pizza's = " + totallarge);// total large pizza
+
+document.write("<br><br> Large pizza's = " + totallarge);
+document.write("<br> total large price: " + largeprice)
 
 
-document.write('<br><br>Total price : ' + total);//total price
+document.write('<br><br>Total price : ' + total);
